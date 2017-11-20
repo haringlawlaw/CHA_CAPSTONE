ab7b6f508f3f26b2700bb22ebc917f0807a52f08
Paolo Javier <paolo.javier@wingaru.com.au>
2017-11-20 14:29:17 +0800

    fixed create and login

--- src/main/java/com/capstone/jmt/controller/MainAppController.java
+++ src/main/java/com/capstone/jmt/controller/MainAppController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -84,7 +85,7 @@ public class MainAppController {
     }
 
     @RequestMapping(value="addUser", method = RequestMethod.POST)
-    public ResponseEntity<?> addUser(@RequestParam User user, @RequestParam String username){
+    public ResponseEntity<?> addUser(@RequestBody User user, @RequestParam String username){
         HashMap<String, Object> response = new HashMap<>();
         User admin = mainService.getUser(username);
         if(null != admin) {
